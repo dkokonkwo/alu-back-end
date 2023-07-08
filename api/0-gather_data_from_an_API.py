b6bf3b5491772f2ab35e57902d246a12f4fa4208
dkokonkwo <d.okonkwo@alustudent.com>
2023-07-08 21:22:14 +0200

    corrections

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -6,6 +6,7 @@ function returns todo info an employee using id from API
 from sys import argv
 import requests
 
+
 def todo_progress(employee_id):
     '''get data from rest api'''
     base_url = "https://jsonplaceholder.typicode.com"
@@ -14,19 +15,23 @@ def todo_progress(employee_id):
 
     try:
         user_response = requests.get(user_endpoint)
-        user_response.raise_for_status() # raises exception for unsuccessful request
+        user_response.raise_for_status()  
+        # above raises exception for unsuccessful request
         user = user_response.json()
-        
+
         response = requests.get(todo_endpoint)
         todos = response.json()
 
         employee_name = user["name"]
-        all_todos = [task for task in todos if task["userId"] == int(employee_id)]
+        all_todos = [
+            task for task in todos if task["userId"] == int(employee_id)
+        ]
         completed_tasks = [todo for todo in all_todos if todo["completed"]]
         total_tasks = len(all_todos)
         total_completed = len(completed_tasks)
 
-        print(f"Employee {employee_name} is done with tasks({total_completed}/{total_tasks}):")
+        print(f"Employee {employee_name} is done with tasks\
+            ({total_completed}/{total_tasks}):")
         for each in all_todos:
             print("\t " + each["title"])
 
